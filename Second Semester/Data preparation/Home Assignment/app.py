27a491fdedbe6d2f6a832678bea68883faea3b4e
autoplug <>
2022-12-30 16:31:54 +0100

    new

--- Second Semester/Data preparation/Home Assignment/app.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-
-df = pd.read_json("capital.json")
-df.info()
-
-data = pd.DataFrame(list(df["data"]),columns=["name","capital","iso2"])
-print(df)
